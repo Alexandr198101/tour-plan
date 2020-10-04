6447bf753b9d6b4ba5d58929b351efa070c08985
Alexandr198101 <sviridovalexander81@gmail.com>
2020-10-04 21:44:29 +0300

    create: PHP mail

--- js/main.js
+++ js/main.js
@@ -34,5 +34,5 @@ menuButton.addEventListener("click", function () {
   console.log("Кликни по кнопке меню");
   document
     .querySelector(".navbar-bottom")
-    .classList.toggle(".navbar-bottom--visible");
+    .classList.toggle("navbar-bottom--visible");
 });
